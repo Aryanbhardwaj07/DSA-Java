ab6c871d2027c90357af598801638f0fb6273df8
Aryanbhardwaj07 <firstnamearyanbhardwaj16@gmail.com>
2023-06-11 22:25:30 +0530

    Mountain of a number(Leetcode)

---
+++ mountain.java
@@ -0,0 +1,23 @@
+
+public class mountain {
+    public static void main(String[] args) {
+    int[]arr={0,2,1,0};
+    int ans=peakIndex(arr);
+        System.out.println(ans);
+    }
+    public static int peakIndex(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid]>arr[mid+1]){
+                end=mid;
+            }else if(arr[mid]<arr[mid+1]){
+                start=mid+1;
+            }
+        }
+        return arr[start];
+    }
+}
+
