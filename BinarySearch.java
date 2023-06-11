74be7cd7b0c246bb67a6c55cb7c4996a74677b7a
Aryanbhardwaj07 <firstnamearyanbhardwaj16@gmail.com>
2023-06-11 12:00:52 +0530

    First commit

---
+++ BinarySearch.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class BinarySearch {
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+
+        }
+        return -1;
+    }
+
+
+        public static void main (String[]args){
+            int[] arr = {2, 3, 6, 7, 8, 23, 45, 69};
+            int target = 6;
+            System.out.println(binarySearch(arr, target));
+        }
+    }
