3d0ab541acd73140c392702f83f21d35da43a862
Aryanbhardwaj07 <firstnamearyanbhardwaj16@gmail.com>
2023-06-11 12:28:50 +0530

    Kid with Max Candies(LEETCODE)

---
+++ MaximumCandies.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class MaximumCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> list =  new ArrayList<>();
+    int max=Integer.MIN_VALUE;
+    for(int i=0;i<candies.length;i++)
+        if(candies[i]>max)
+            max=candies[i];
+    
+    for(int i=0;i<candies.length;i++)
+    {
+        if(candies[i]+extraCandies>=max)
+            list.add(true);
+        else
+            list.add(false);
+    }
+    return list;
+    }
+}
