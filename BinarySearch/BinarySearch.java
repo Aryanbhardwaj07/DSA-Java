3bb38657c6ba9c61d7eb986570dd427337af2eca
ARYAN BHARDWAJ <80642649+Aryanbhardwaj07@users.noreply.github.com>
2023-06-23 22:23:59 +0530

    Update BinarySearch.java

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -3,6 +3,7 @@ public class BinarySearch {
         int start = 0;
         int end = arr.length - 1;
         while (start <= end) {
+            
             int mid = start + (end - start) / 2;
             if (target < arr[mid]) {
                 end = mid - 1;
