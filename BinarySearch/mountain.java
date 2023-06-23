7d07add0e39bc16ac1eb044c88fbd219358f4cba
ARYAN BHARDWAJ <80642649+Aryanbhardwaj07@users.noreply.github.com>
2023-06-23 22:24:41 +0530

    Update mountain.java

--- BinarySearch/mountain.java
+++ BinarySearch/mountain.java
@@ -8,6 +8,7 @@ public class mountain {
     public static int peakIndex(int[] arr){
         int start=0;
         int end=arr.length-1;
+        
 
         while(start<end){
             int mid = start + (end - start) / 2;
