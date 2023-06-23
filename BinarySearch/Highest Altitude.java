2a062a5cd031b5b33fc30695bd55006b953ae43d
ARYAN BHARDWAJ <80642649+Aryanbhardwaj07@users.noreply.github.com>
2023-06-23 22:24:31 +0530

    Update Highest Altitude.java

--- BinarySearch/Highest Altitude.java
+++ BinarySearch/Highest Altitude.java
@@ -7,7 +7,8 @@ public class mountain {
      static public int largestValue(int[]gain){
         int max=0;
         int current=0;
-        for(int i=0;i<gain.length;i++){
+        
+         for(int i=0;i<gain.length;i++){
             current+=gain[i];
             max=Math.max(current,max);
         }
