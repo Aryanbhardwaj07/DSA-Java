688ac03923aa80cc3c4b15f96c002fb5b9762253
ARYAN BHARDWAJ <80642649+Aryanbhardwaj07@users.noreply.github.com>
2023-06-24 21:03:09 +0530

    Update insertion_sort.java

--- Insertion Sort/insertion_sort.java
+++ Insertion Sort/insertion_sort.java
@@ -9,6 +9,7 @@ public class mountain {
     }
  static void insertion(int[]arr){
         for(int i=0;i<arr.length-1;i++){
+
             for (int j = i+1; j >0 ; j--) {
                 if(arr[j]<arr[j-1]){
                     swapArr(arr,j,j-1);
